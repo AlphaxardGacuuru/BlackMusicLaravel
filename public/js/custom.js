253325e401a126208d2351428e5b10a5c1586f93
ALphaxard <alphaxardgacuuru47@gmail.com>
2021-05-10 13:56:06 +0300

    Offline sw

--- public/js/custom.js
+++ public/js/custom.js
@@ -103,7 +103,9 @@ function copyText() {
 
 //Register service worker
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/sw.js')
-		.then((reg) => console.log('Service worker registered', reg))
-		.catch((err) => console.log('Service worker not registered', err));
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('/sw.js')
+			.then((reg) => console.log('Service worker registered', reg))
+			.catch((err) => console.log('Service worker not registered', err));
+	})
 }
--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,15 @@
-// staticCacheName needs to be changed after any of the assets are changed
-// const staticCacheName = 'site-static-v1';
+// // staticCacheName needs to be changed after any of the assets are changed
+const staticCacheName = 'site-static-v2';
 // const dynamicCacheName = 'site-dynamic-v1';
-// Assests to be cached
+// // Assests to be cached
 // const assets = [
 // 	'/',
 // 	'/posts',
+// 	'/video-charts/newlyReleased/All',
+// 	// '/search',
+// 	'/cart',
+// 	'/library',
+// 	'/home',
 // 	'manifest.webmanifest',
 // 	'css/app.css',
 // 	'css/custom.css',
@@ -14,10 +19,9 @@
 // 	'storage/img/musical-note.png',
 // 	'https://fonts.googleapis.com/css?family=Nunito',
 // 	'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700',
-// 	'/home',
 // ];
 
-// Cache size limit function
+// // Cache size limit function
 // const limitCacheSize = (name, size) => {
 // 	caches.open(name).then(cache => {
 // 		cache.keys().then(keys => {
@@ -28,52 +32,57 @@
 // 	})
 // };
 
-// install event
-// self.addEventListener('install', evt => {
-// 	//console.log('service worker installed');
-// 	// Function to prevent the install event from stopping the async methods from finishing 
-// 	evt.waitUntil(
-// 		// Cache assets for faster page loading and offline mode
-// 		caches.open(staticCacheName).then(cache => {
-// 			console.log('caching shell assets');
-// 			cache.addAll(assets);
-// 		})
-// 	);
-// });
+// Install event
+self.addEventListener('install', e => {
+	console.log('Service Worker installed');
+	// Function to prevent the install event from stopping the async methods from finishing 
+	// e.waitUntil(
+	// 	// 	Cache assets for faster page loading and offline mode
+	// 	caches.open(staticCacheName)
+	// 		.then(cache => {
+	// 			console.log('Caching files');
+	// 			cache.addAll(assets);
+	// 		})
+	// 		.then(() => self.skipWaiting())
+	// );
+});
 
-// activate event
-// self.addEventListener('activate', evt => {
-// 	//console.log('service worker activated');
-// 	// Function to prevent the install event from stopping the async methods from finishing 
-// 	evt.waitUntil(
-// 		// Get cached assests based on keys
-// 		caches.keys().then(keys => {
-// 			//console.log(keys)
-// 			// Returns multiple promises 
-// 			return Promise.all(
-// 				//for each return check whether the key of the already cached assests is present if not delete 
-// 				keys.filter(key => key !== staticCacheName && key !== dynamicCacheName)
-// 					.map(key => caches.delete(key))
-// 			)
-// 		})
-// 	);
-// });
+// Activate event
+self.addEventListener('activate', e => {
+	console.log('Service Worker Activated');
+	// Function to prevent the install event from stopping the async methods from finishing 
+	e.waitUntil(
+		// Get cached assests based on keys
+		caches.keys().then(staticCacheNames => {
+			// Returns multiple promises 
+			return Promise.all(
+				//for each return check whether the key of the already cached assests is present if not delete 
+				staticCacheNames.map(cache => {
+					if (cache !== staticCacheName) {
+						console.log('Service Worker: Clearing all cache');
+						return caches.delete(cache);
+					}
+				})
+			);
+		})
+	);
+});
 
-// fetch event
-// self.addEventListener('fetch', evt => {
-// 	// console.log('fetch event', evt);
-// 	evt.respondWith(
-// 		// Checks whether the resources being fetched are already cached and fetching those instead
-// 		caches.match(evt.request).then(cacheRes => {
-// 			// If resources are not cached and are obtainded from the server the add those to dynamic cache
-// 			return cacheRes || fetch(evt.request).then(fetchRes => {
-// 				return caches.open(dynamicCacheName).then(cache => {
-// 					cache.put(evt.request.url, fetchRes.clone());
-// 					//limitCacheSize(dynamicCacheName, 100);
-// 					return fetchRes;
-// 				})
-// 			})
-// 			// Redirect to fallback page if page is not cached
-// 		}).catch(() => caches.match('/home'))
-// 	);
-// });
+// Fetch event
+self.addEventListener('fetch', e => {
+	console.log('Fetching')
+	e.respondWith(
+		fetch(e.request)
+		.then(res => {
+			// Make clone of response
+			const resClone = res.clone();
+			// Open cache
+			caches.open(staticCacheName)
+			.then(cache => {
+				// Add response to cache
+				cache.put(e.request, resClone);
+			});
+			return res
+		}).catch(err => cache.match(e.request).then(res => res))
+	)
+})
